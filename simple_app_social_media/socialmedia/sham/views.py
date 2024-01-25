e5a925bbe2ac8b771cd5db97b5fb63fe8ee6bd7b
SamG1234 <np03cs4s210297@heraldcollege.edu.np>
2024-01-25 22:31:39 +0545

    backend

--- simple_app_social_media/socialmedia/sham/views.py
+++ simple_app_social_media/socialmedia/sham/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth  import authenticate, login
 
 # Create your views here.
+
+def signin(request):
+    # if data is posted to signin and goes through authentication
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, login)
+            return redirect('index')
+    
+    else:
+        if request.user.is_authenticated:
+            return redirect('index')
+        return render(request, 'signin.html')
+
