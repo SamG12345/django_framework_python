3347f85cdb697ccaf94ac748687be4a2d3a33bfb
SamG1234 <np03cs4s210297@heraldcollege.edu.np>
2024-01-26 13:50:54 +0545

    updated model

--- simple_app_social_media/socialmedia/sham/admin.py
+++ simple_app_social_media/socialmedia/sham/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Lekh
 
 # Register your models here.
 
@@ -7,5 +7,8 @@ from .models import Profile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user']
 
+@admin.register(Lekh)
+class lekhAdmin(admin.ModelAdmin):
+    list_display = ['body']
 # Note: 'user' in list_display assumes you have a __str__ method in your User model.
 # If not, you might want to display a different attribute or define __str__ in your User model.
--- simple_app_social_media/socialmedia/sham/models.py
+++ simple_app_social_media/socialmedia/sham/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 
+# User Profile
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     companian = models.ManyToManyField("self", related_name="companianed_by", symmetrical=False, blank=True)
@@ -18,3 +19,15 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile = Profile(user = instance)
         user_profile.save()
 post_save.connect(create_profile, sender=User)
+
+# Written byt profile
+class Lekh(models.Model):
+    profile = models.ForeignKey(Profile, on_delete = models.CASCADE)
+    body = models.CharField(max_length=200)
+    date_created = models.DateTimeField(Profile, auto_now_add=True)
+    likes = models.ManyToManyField(Profile, related_name="liked_by", symmetrical=False, blank=True)
+
+    def no_of_likes(self):
+        return self.likes.count()
+    def __str__(self) -> str:
+        return self.body
