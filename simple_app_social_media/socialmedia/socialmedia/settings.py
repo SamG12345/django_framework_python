ad8dab88c013614bcfbf7bd31832cc39e41beb40
SamG1234 <np03cs4s210297@heraldcollege.edu.np>
2024-01-27 13:50:56 +0545

    setting

--- simple_app_social_media/socialmedia/socialmedia/settings.py
+++ simple_app_social_media/socialmedia/socialmedia/settings.py
@@ -119,9 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'king/static')
+    os.path.join(BASE_DIR, 'sham/static')
 ]
 
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
