acd3f711d0625decd1b8a29f15ef9f44ab1efe01
SamG1234 <np03cs4s210297@heraldcollege.edu.np>
2024-01-28 00:15:00 +0545

    setting

--- simple_app_social_media/socialmedia/socialmedia/settings.py
+++ simple_app_social_media/socialmedia/socialmedia/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIR = [
 ]
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'sham/media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
