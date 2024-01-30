7cd223be4f6b69389a88c967ca802baa084d26d2
SamG1234 <np03cs4s210297@heraldcollege.edu.np>
2024-01-30 23:25:47 +0545

    update

--- simple_app_social_media/socialmedia/socialmedia/settings.py
+++ simple_app_social_media/socialmedia/socialmedia/settings.py
@@ -76,10 +76,6 @@ WSGI_APPLICATION = 'socialmedia.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
 
 
