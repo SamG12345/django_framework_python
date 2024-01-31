fb9c0ec4f58ea5f559d1c7a56a6768f54aff9df8
SamG1234 <np03cs4s210297@heraldcollege.edu.np>
2024-01-31 14:13:42 +0545

    app

--- simple_app_social_media/socialmedia/socialmedia/settings.py
+++ simple_app_social_media/socialmedia/socialmedia/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'socialmedia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '-*gG635bgFG-561B4bBG2GaadCaa-ggC',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '46117',
     }
 }
 
