d8de2947f671dba5b614243af0d46409de483f43
SamG1234 <np03cs4s210297@heraldcollege.edu.np>
2024-01-31 14:37:26 +0545

    app

--- simple_app_social_media/socialmedia/build.sh
+++ simple_app_social_media/socialmedia/build.sh
@@ -1,2 +1,13 @@
-pppip install -r requirements.txt
-python 3.9 manage.py collectstatic
+#!/bin/bash
+# Create and activate the virtual environment
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
+# Install SQLite development library
+pip install django
+
+# Install requirements
+python3 -m pip install -r requirements.txt
+
+# Collect static files
+python3 manage.py collectstatic
