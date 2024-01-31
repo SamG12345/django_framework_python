5c5d3cfe9974723cb8a39fb52e88ef7c7b50b934
SamG1234 <np03cs4s210297@heraldcollege.edu.np>
2024-01-31 14:53:24 +0545

    app

--- simple_app_social_media/socialmedia/build.sh
+++ simple_app_social_media/socialmedia/build.sh
@@ -1,14 +1,3 @@
-
-# Upgrade pip
-python3 -m pip install --upgrade pip
-
-pip install python
-
-# Create and activate the virtual environment
-python3 -m venv venv
-source venv/bin/activate
-
-
 # Install requirements
 python3 -m pip install -r requirements.txt
 
